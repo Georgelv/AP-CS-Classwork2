8b18e074d2c0520dcb4c3588198acc243988187a
George Lyu <929019495@qq.com>
2018-03-06 19:41:48 +0800

    2018.3.6 Library Homework

--- library/Library.java
+++ library/Library.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * George Lyu S3C7
+ APCS
+ Mr.Daniel
+ This is a program about library System.
  */
 package library;
 
