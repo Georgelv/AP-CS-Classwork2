963eba627439b671ea80f07e07d81f4debdaf497
George Lyu <929019495@qq.com>
2018-03-07 09:17:59 +0800

    2018.3.7 Library Project

--- library/Library.java
+++ library/Library.java
@@ -1,25 +1,24 @@
 /*
- * George Lyu S3C7
+ * George Lyu
+ S3C7
  APCS
- Mr.Daniel
- This is a program about library System.
+ This program aims for organizing library.
  */
 package library;
 
 import java.util.Date;
 
 /**
- * @author Daniel Gunn
- * Please implement the Book, LibraryItem and CD classes
+ *
  */
     
     public class Library{
 
     public static void main(String[] args) {
-        Book b = new Book("12 Rules for Life", "Jordan Peterson", 123, false, new Date(), false );
+        item b = new Book("12 Rules for Life", "Jordan Peterson", 123, false, new Date(), false );
         b.printDetails();
         
-        CD cd = new CD("And Justice for all", "Metallica", 567, false, new Date(), false, "Metal");
+        item cd = new CD("And Justice for all", "Metallica", 567, false, new Date(), false, "Metal");
         cd.printDetails();
         
 }}
